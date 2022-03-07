79ac885e38d28b9abe85c659632342588c7e3a4b
zero71029 <wiz71029@gmail.com>
2022-03-07 08:06:45 +0800

    ZeroTool

---
+++ src/main/java/com/jetec/CRM/Tool/ZeroTools.java
@@ -0,0 +1,286 @@
+package com.jetec.CRM.Tool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Properties;
+import java.util.UUID;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+
+@Component
+public class ZeroTools {
+
+// 筆記
+	///////////////////////////////////////////
+//	返回上一頁
+//	window.history.back();location.reload();
+////////////////////
+// sql修改为不自动更新（只是插入是自动生成当前时间）
+//alter table hello change uptime uptime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP	
+	//////////////////////////////////////
+//	重置 MySQL 自增列 AUTO_INCREMENT 初時值
+//	ALTER TABLE tbl AUTO_INCREMENT = 1;
+//////////////////////////////////
+//	一键复制至剪切板
+//	url.select();
+//    document.execCommand("Copy");
+//////jpa排序
+	// Sort sort = Sort.by(Direction.DESC,"marketid");
+	// return mr.findAll( sort);
+///////////////////////////////
+//	HttpServletRequest sce
+//	ServletContext app = sce.getServletContext();
+//	app.setAttribute("billboardgroup", bgr.findAll());
+//////////ajax
+//	var formData = new FormData($(".uppdf")[0]);
+//	$.ajax({
+//        url: '${pageContext.request.contextPath}/CRM/selectContactByClientName/' + $("input[name='client']").val(),//接受請求的Servlet地址
+//        type: 'POST',
+//        // data: formData,
+//        // async: false,//同步請求
+//        // cache: false,//不快取頁面
+//        // contentType: false,//當form以multipart/form-data方式上傳檔案時，需要設定為false
+//        // processData: false,//如果要傳送Dom樹資訊或其他不需要轉換的資訊，請設定為false
+//        success: function (json) {
+//            $(".CCC").empty();
+//
+//            for (var j of json) {
+//                console.log(j);
+//
+//                $(".CCC").append('<div class="row TTT" onclick="clickContact(`' + j.name + '`,`' + j.phone + '`,`' + j.moblie + '`)">' +
+//                    '<div class="col-md-1"></div>' +
+//                    '<div class="col-md-3">' + j.name + '</div>' +
+//                    '<div class="col-md-3">' + j.phone + '</div>' +
+//                    '<div class="col-md-3">' + j.moblie + '</div>' +
+//                    '</div>');
+//            }
+//        },
+//        error: function (returndata) {
+//            console.log(returndata);
+//        }
+//    });
+//////////////////////////////////////////////////////////////////
+	/*
+	 *
+	 * @mailTo 收件人
+	 * 
+	 * @text 郵件內文
+	 * 
+	 * @Subject 郵件的標題
+	 * 
+	 * @maillist 群發郵件
+	 *
+	 */
+	private String STMP = "";
+	private String secret = "";
+
+	// 郵件
+	public void mail(String mailTo, String text, String Subject, String maillist) {
+
+//		mailTo = "wiz71028@hotmail.com";
+		new Thread() {
+			@Override
+			public void run() {
+				System.out.println(mailTo);
+				System.out.println(text);
+
+				Properties prop = new Properties();
+				// 發件人的郵箱的SMTP 服務器地址（不同的郵箱，服務器地址不同，如139和qq的郵箱服務器地址不同）
+				prop.setProperty("mail.host", "smtp.gmail.com");
+				// 使用的協議（JavaMail規範要求）
+				prop.setProperty("mail.transport.protocol", "smtp");
+				// 需要請求認證
+				
+				prop.setProperty("mail.smtp.auth", "true");
+				prop.put("mail.smtp.starttls.enable", "true");
+				prop.put("mail.smtp.port", "587");
+				prop.put("mail.smtp.ssl.trust","smtp.gmail.com");
+				// 使用JavaMail發送郵件的5個步驟
+				// 1、創建session
+				Session session = Session.getInstance(prop);
+				// 開啟Session的debug模式，這樣就可以查看到程序發送Email的運行狀態
+				session.setDebug(true);
+				Transport ts = null;
+				try {
+					// 2、通過session得到transport對象
+					ts = session.getTransport();
+					// 3、使用郵箱的用戶名和密碼連接郵件服務器
+					// 發送郵件時，發件人需要提交郵箱的用戶名和密碼給smtp服務器，用戶名和密碼都通過驗證之後才能夠正常發送郵件給收件人。
+					ts.connect("smtp.gmail.com", "jeter.tony56@gmail.com", STMP);
+					// 4、創建郵件
+//	            Message message = createComplexMail(session); 
+					MimeMessage message = new MimeMessage(session);
+					// 指明發件人
+					message.setFrom(new InternetAddress("AAA"));
+					// 指明收件人
+					message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
+					message.addRecipients(Message.RecipientType.BCC, maillist);
+					// 郵件的標題
+					message.setSubject(Subject);
+					// 郵件的文本內容
+					message.setContent(text, "text/html;charset=UTF-8");
+
+					// 5、發送郵件
+
+					ts.sendMessage(message, message.getAllRecipients());
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						// 關閉transport對象
+						ts.close();
+					} catch (MessagingException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}.start();
+
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// google登入
+	public void oauth(String idtoken) {
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+				// Specify the CLIENT_ID of the app that accesses the backend:
+				.setAudience(Collections
+						.singletonList("849367464998-0c4najofsqmh3rteejq2dc3va9iqdps2.apps.googleusercontent.com"))
+				// Or, if multiple clients access the backend:
+				// .setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+				.build();
+
+		// (Receive idTokenString by HTTPS POST)
+
+		GoogleIdToken idToken = null;
+		try {
+			idToken = verifier.verify(idtoken);
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (idToken != null) {
+			Payload payload = idToken.getPayload();
+
+			// Print user identifier
+			String userId = payload.getSubject();
+			System.out.println("User ID: " + userId);
+
+			// Get profile information from payload
+			String email = payload.getEmail();
+//			boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+			String name = (String) payload.get("name");
+//			String pictureUrl = (String) payload.get("picture");
+			String locale = (String) payload.get("locale");
+//			String familyName = (String) payload.get("family_name");
+			String givenName = (String) payload.get("given_name");
+			System.out.println("User name: " + name);
+			System.out.println("User email: " + email);
+			System.out.println("User locale: " + locale);
+			System.out.println("User givenName: " + givenName);
+
+			// Use or store profile information
+			// ...
+
+		} else {
+			System.out.println("Invalid ID token.");
+		}
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	// 機器人判斷
+	public boolean recaptcha(String toke) {
+
+		// 取得token
+		System.out.println(toke);
+		System.out.println("*****recaptcha******");
+
+//		JSONObject obj = new JSONObject();
+//		obj.put("secret", "");
+//		obj.put("response", body);
+//		obj.put("remoteip", ip);
+		try {
+			// 編輯google需要文件
+			String url = "https://www.google.com/recaptcha/api/siteverify",
+					params = "secret=" + secret + "&response=" + toke;
+			// 開啟網路發送
+			HttpURLConnection http = (HttpURLConnection) new URL(url).openConnection();
+			http.setDoOutput(true);
+			http.setRequestMethod("POST");
+			http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			OutputStream out = http.getOutputStream();
+			out.write(params.getBytes("UTF-8"));
+			out.flush();
+			out.close();
+			
+			// 接收返回資料
+			InputStream res = http.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(res, "UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			int cp;
+			while ((cp = rd.read()) != -1) {
+				sb.append((char) cp);
+			}
+			JSONObject json = new JSONObject(sb.toString());
+			System.out.println(json);
+			res.close();
+
+			// 判斷成功存在session
+			if ((boolean) json.get("success")) {
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	/**
+	 * 獲得一個UUID
+	 * 
+	 * @return String UUID
+	 */
+	public String getUUID() {
+		String s = UUID.randomUUID().toString();
+		// 去掉“-”符號
+		return s.substring(0, 8) + s.substring(9, 13) + s.substring(14, 18) + s.substring(19, 23) + s.substring(24);
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+//時間格式畫
+	public String getTime(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		return sdf.format(date);
+	}
+
+}
